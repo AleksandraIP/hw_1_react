90fec8ff3017c6fd244dcf1060d4cced9ded552a
AleksandraIP <85641733+AleksandraIP@users.noreply.github.com>
2022-10-20 02:03:17 +0300

    second

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Oleksandra Popova</h1>
-      <h3>063 610 54 17</h3>
+      <h3>079 111 11 11</h3>
       <h3>aleksandraip@ukr.net</h3>
 
         <div className="Country">
